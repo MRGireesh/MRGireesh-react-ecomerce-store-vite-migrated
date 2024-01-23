da8098fa357e0b5990307007df403cdb25298b66
GireeshMR <84763693+GireeshMR@users.noreply.github.com>
2024-01-23 16:02:04 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      'sm' : '640px',
+      'md' : '778px',
+      'lg' : '1024px',
+      'xl' : '1280px'
+    },
+    
+    extend: {
+      fontFamily: {
+        'inter': ['inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+});
