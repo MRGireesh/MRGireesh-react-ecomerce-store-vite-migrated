2580282bebb9b3cec2ee7967468b7b4deca55a64
Gireesh M R <gireeshmr.int@outlook.com>
2024-02-01 21:57:55 +0530

    added vercel analytics (#2) (#3)
    Co-authored-by: GireeshMR <84763693+GireeshMR@users.noreply.github.com>

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -3,7 +3,9 @@ import { Header } from "../Header/Header";
 import { NavigationButtons } from "../NavigationButtons/NavigationButtons";
 import { Footer } from "../Footer/Footer";
 import { ProductBar } from "../ProductBar/ProductBar";
-
+import { inject } from '@vercel/analytics';
+ 
+inject();
 export const Main = () => {
   return (
     <div>
